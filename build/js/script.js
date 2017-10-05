58ea5e57ac0810dff8b91b8746fcc3b0248eb0c0
Eugene Fedyai <fedyai.eugen@gmail.com>
2017-10-05 18:52:26 +0300

    Finished

--- build/js/script.js
+++ build/js/script.js
@@ -25,14 +25,17 @@ $('#accordion').accordion({
   active: false,
   collapsible: true,
   icons: {
-    header: 'features__caption--plus',
-    activeHeader: 'features__caption--minus'
+    header: 'responsive__caption--plus',
+    activeHeader: 'responsive__caption--minus'
   }
 });
 
 $('.banner__dots').hover(
   function() {
-    $('.banner__category').animate({
+    $(this).stop().animate({
+      'background-color': 'rgb(255, 228, 0)'
+    });
+    $('.banner__category').stop().animate({
         'background-color': 'rgb(255, 255, 255)'
       },
       500,
@@ -40,6 +43,9 @@ $('.banner__dots').hover(
     );
   },
   function() {
+    $(this).stop().animate({
+      'background-color': 'rgb(0, 0, 0)'
+    });
     $('.banner__category').stop().animate({
         'background-color': 'rgb(255, 228, 0)'
       },
@@ -65,28 +71,106 @@ $('.gallery__item:first').click();
 
 $('.service__decoration').hover(
   function() {
-    $(this).animate({
+    $(this).stop().animate({
         'bottom': '-300px',
         'left': '-100px',
-        'color': 'rgb(237, 237, 237)',
+        'color': 'rgba(255, 228, 0, 0.7)',
         'opacity': 1
       },
       1000
     );
   },
   function() {
-    $(this).animate({
+    $(this).stop().animate({
         'bottom': '-500px',
         'left': '-300px',
-        'color': 'rgba(255, 228, 0, 0.7)'
+        'color': 'rgb(237, 237, 237)'
       },
       1000
     );
   }
 );
 
-$('.price__item-body').hide();
+$('.price__list').hide();
+var priceArrow = $('.price__arrow');
+priceArrow.show();
+priceArrow.addClass('price__arrow--closed');
+priceArrow.click(function() {
+  var priceList = $(this).next('.price__list');
+  if (priceList.is(':hidden')) {
+    priceList.slideDown();
+    $(this).addClass('price__arrow--opened');
+    $(this).removeClass('price__arrow--closed');
+  } else {
+    priceList.slideUp();
+    $(this).addClass('price__arrow--closed');
+    $(this).removeClass('price__arrow--opened');
+  }
+});
 
-$('.price__arrow').click(function() {
-  $('.price__item-body').slideToggle(500);
+var teamInfo = $('.team__info');
+
+$('.team__info').hide();
+
+$('.team__box').hover(
+  function() {
+    var teamInfo = $(this).children('.team__info');
+    teamInfo.stop().fadeIn();
+  },
+  function() {
+    teamInfo.stop().fadeOut();
+  }
+);
+
+$('.news__item').hover(
+  function() {
+    var readMore = $(this).children('.news__more');
+    readMore.addClass('news__more--active');
+    $(this).addClass('news__item--active');
+  },
+  function() {
+    var readMore = $(this).children('.news__more');
+    readMore.removeClass('news__more--active');
+    $(this).removeClass('news__item--active');
+  }
+);
+
+$('.contact__form').validate({
+  rules: {
+    name: 'required',
+    email: {
+      required: true,
+      email: true
+    },
+    lastName: 'required',
+    letter: {
+      required: true,
+      maxlength: [5, 300]
+    }
+  },
+  messages: {
+    name: {
+      required: "Please type the name you'd like to use."
+    },
+    letter: {
+      maxlength: "Your text must be no more than 300 characters long."
+    }
+  }
+});
+
+var map = $('.map iframe');
+map.hide();
+var mapText = $('.map__text');
+mapText.addClass('map__text--closed');
+
+$('.map__container').click(function() {
+  if (map.is(':hidden')) {
+    map.slideDown();
+    mapText.addClass('map__text--opened');
+    mapText.removeClass('map__text--closed');
+  } else {
+    map.slideUp();
+    mapText.addClass('map__text--closed');
+    mapText.removeClass('map__text--opened');
+  }
 });
